541b2d03c06fe382d8d76da6fee2d428606d0e67
Berhan-Adhana <berhanghe90@gmail.com>
2026-01-03 19:52:42 -0700

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,54 +1,80 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { AlertDialog, Box, Button, Flex } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import axios from "axios";
+import { use, useState } from "react";
+import { set } from "zod";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          <TrashIcon />
-          Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth="450px">
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description size="2">
-          Are you sure you want to delete this issue?
-        </AlertDialog.Description>
+  const [error, setError] = useState(false);
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              variant="solid"
-              color="red"
-              onClick={(e) => {
-                axios
-                  .delete("/api/issues/" + issueId)
-                  .then(() => {
-                    router.push("/issues");
-                    router.refresh();
-                  })
-                  .catch((error) => {
-                    console.error("Failed to delete the issue:", error.message);
-                  });
-              }}
-            >
-              Confirm Delete
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const deleteIssue = () => {
+    axios
+      .delete("/api/issues/" + issueId)
+      .then(() => {
+        router.push("/issues");
+        router.refresh();
+      })
+      .catch((error) => {
+        setError(true);
+        console.error("Failed to delete the issue:", error.message);
+      });
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            <TrashIcon />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this issue?
+          </AlertDialog.Description>
+
+          <Box mt="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={deleteIssue}>
+                Confirm Delete
+              </Button>
+            </AlertDialog.Action>
+          </Box>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Error </AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            This issue cannot be deleted.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Action>
+              <Button
+                variant="soft"
+                color="gray"
+                onClick={() => setError(false)}
+              >
+                OK
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
