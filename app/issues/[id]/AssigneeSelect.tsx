07c53c832014d55a3d020ba638dab4cf551d6e73
Berhan-Adhana <berhanghe90@gmail.com>
2026-01-14 06:59:39 -0700

    Populate the Assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,14 +1,22 @@
+import { prisma } from "@/prisma/prisma";
 import { Select } from "@radix-ui/themes";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = async () => {
+  const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
+
   return (
     <Select.Root defaultValue="Select Assignee">
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="Select Assignee"> Select Assignee</Select.Item>
-          <Select.Item value="berhan"> Berhan</Select.Item>
+          <Select.Item value=" ">UnAssign</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+          {/* <Select.Item value="berhan"> Berhan</Select.Item> */}
         </Select.Group>
       </Select.Content>
     </Select.Root>
