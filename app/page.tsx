fed29df1cc129856f4974ae075e8219ca2d00889
Berhan-Adhana <berhanghe90@gmail.com>
2026-01-17 16:23:14 -0700

    Build IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import LatestIssues from "./LatestIssues";
+import { prisma } from "@/prisma/prisma";
+import { Grid, Flex } from "@radix-ui/themes";
+import IssueSummary from "./IssueSummary";
 
-const Home = () => {
+const Home = async () => {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
-    <div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        {/* <IssueChart
+          open={open}
+          inProgress={inProgress}
+          closed={closed}
+        /> */}
+      </Flex>
       <LatestIssues />
-    </div>
+    </Grid>
   );
 };
 
