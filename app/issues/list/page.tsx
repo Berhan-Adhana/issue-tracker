178ac323b9cd91bd3c7d478e22da6a12325f4636
Berhan-Adhana <berhanghe90@gmail.com>
2026-01-17 07:58:08 -0700

    make column sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,12 +1,14 @@
 import { prisma } from "@/prisma/prisma";
-import { Link, Table, Text } from "@radix-ui/themes";
+import { Table, Text } from "@radix-ui/themes";
 import { IssueStatusBadge } from "@/app/components";
 import IssueActions from "./IssueActions";
 import { Status } from "@/app/generated/prisma/enums";
 import { Issue } from "@/app/generated/prisma/client";
+import NextLink from "next/link";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams?: { status?: string };
+  searchParams?: { status?: string; orderBy?: keyof Issue };
 }
 const IssuesPage = async ({ searchParams }: Props) => {
   const statusParam = await searchParams;
@@ -15,7 +17,14 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? (statusParam?.status as Status)
     : undefined;
 
-  console.log("Search params:", status);
+  console.log("The status is: ", status);
+
+  const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Create", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const data: Issue[] = await prisma.issue.findMany({
     where: { status },
   });
@@ -26,9 +35,20 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root className="mt-5" layout={"auto"} variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            {columns.map((col) => (
+              <Table.ColumnHeaderCell key={col.label} className={col.className}>
+                <NextLink
+                  href={`/issues/list?status=${status ?? ""}&orderBy=${
+                    col.value
+                  }`}
+                >
+                  {col.label}
+                </NextLink>
+                {col.value === statusParam?.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
@@ -38,7 +58,9 @@ const IssuesPage = async ({ searchParams }: Props) => {
             data.map((issue) => (
               <Table.Row key={issue.id.toFixed()}>
                 <Table.Cell>
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                  <NextLink href={`/issues/${issue.id}`}>
+                    {issue.title}
+                  </NextLink>
                   <div className="block md:hidden">
                     <IssueStatusBadge status={issue.status} />
                   </div>
