c5b556c96bb3152db2517d5d8b7104e322fbb55e
Berhan-Adhana <112740809+Berhan-Adhana@users.noreply.github.com>
2026-01-17 19:47:42 -0700

    Reduce connection limit for safer development

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -11,7 +11,7 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
-  connectionLimit: 10, // ⬆ safer for dev
+  connectionLimit: 2, // ⬆ safer for dev
    ssl: {
     rejectUnauthorized: true,
   },
